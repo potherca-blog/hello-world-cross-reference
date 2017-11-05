2d92c3c81a47c31444933715f4e433e668d5f333
Ben Peachey <potherca@hotmail.com>
2017-11-05 21:54:54 +0000

    Adds static HTML version.

--- src/Printer/HtmlPrinter.php
+++ src/Printer/HtmlPrinter.php
@@ -89,7 +89,9 @@ HTML;
 
 
 $html = <<<HTML
-<html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
